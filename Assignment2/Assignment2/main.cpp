dc2ffc09e824675d5ad62b951623ebd1e1f75455
Türkü <turku@tarentum.io>
2021-05-07 05:30:35 +0300

    change naming

--- Assignment2/Assignment2/main.cpp
+++ Assignment2/Assignment2/main.cpp
@@ -26,7 +26,7 @@ void rubiksCube() {
                 cubeCreator( cube, color );
                 
                 // Cube translation
-                mat4 translation = Translate( vec3(i-0.5, j-0.5, k-0.5) );
+                mat4 translation = Translate( vec3(i - 0.5, j - 0.5, k - 0.5) );
                 for (int i = 0; i < numOneCubeVertices ; i++) {
                     cube[i] = translation * cube[i];
                 }
@@ -46,31 +46,31 @@ void rubiksCube() {
     }
 }
 
-// MARK: - Rotate S
-// Rotate face by angle delta
-void cubeSideRotation( int delta ) {
-    int currPos = 0;
+// MARK: - Rotate the cube
+// Rotate face by angle
+void rotateCube( int angle ) {
+    int currentPos = 0;
 
     // Update rotation matrix
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
             if (rotationAxis == axisX) {
-                currPos = currentCubePos[currentBlock][i][j];
-                rotationMatrix[currPos] = RotateX(delta) * rotationMatrix[currPos];
+                currentPos = currentCubePos[currentBlock][i][j];
+                rotationMatrix[currentPos] = RotateX(angle) * rotationMatrix[currentPos];
             } else if (rotationAxis == axisY) {
-                currPos = currentCubePos[i][currentBlock][j];
-                rotationMatrix[currPos] = RotateY(delta) * rotationMatrix[currPos];
+                currentPos = currentCubePos[i][currentBlock][j];
+                rotationMatrix[currentPos] = RotateY(angle) * rotationMatrix[currentPos];
             } else if (rotationAxis == axisZ) {
-                currPos = currentCubePos[i][j][currentBlock];
-                rotationMatrix[currPos] = RotateZ(delta) * rotationMatrix[currPos];
+                currentPos = currentCubePos[i][j][currentBlock];
+                rotationMatrix[currentPos] = RotateZ(angle) * rotationMatrix[currentPos];
             }
-            glUniformMatrix4fv(vRotation[currPos], 1, GL_TRUE, rotationMatrix[currPos]);
+            glUniformMatrix4fv(vRotation[currentPos], 1, GL_TRUE, rotationMatrix[currentPos]);
         }
     }
 
-    rotatePhase = rotatePhase + delta;
+    rotateAngle = rotateAngle + angle;
     glutPostRedisplay();
-    if (rotatePhase == 90) {
+    if (rotateAngle == 90) {
         for (int k = 0 ; k < 2 ; k++) {
             for (int l = 0 ; l < 2 ; l++) {
                 for (int m = 0 ; m < 2 ; m++) {
@@ -78,15 +78,15 @@ void cubeSideRotation( int delta ) {
                 }
             }
         }
-        rotatePhase = 0;
+        rotateAngle = 0;
     } else {
-        glutTimerFunc(animationSpeed, cubeSideRotation, delta);
+        glutTimerFunc(animationSpeed, rotateCube, angle);
     }
 }
 
-// MARK: - Rotate Rubiks Cube
+// MARK: - Set next positions
 
-void rubiksCubeRotation( int direction, int subcube, int axis ) {
+void setNextPositions( int direction, int subcube, int axis ) {
     int rotation[2][2];
     if (direction < 0) {
         for (int i = 0 ; i < 2 ; i++) {
@@ -114,7 +114,7 @@ void rubiksCubeRotation( int direction, int subcube, int axis ) {
         }
     }
 
-    /*copy back the transposed subcube to nextCubePos*/
+    // Copy subcube back to nextCubePos
     for (int i = 0 ; i < 2 ; i++) {
         for (int j = 0 ; j < 2 ; j++) {
             if (axis == axisX) {
@@ -130,22 +130,22 @@ void rubiksCubeRotation( int direction, int subcube, int axis ) {
 
 // MARK: - Set current block, rotation axis and rotation
 
-int rotateAlong( int axis, int nextA, int nextB, int key ) {
-    int rotation = 0;
+int setParameters( int axis, int firstAxis, int secondAxis, int key ) {
+    int direction = 0;
 
     // Rotate along X
     if (axis == axisX) {
-        if (nextB == 0) {
+        if (secondAxis == 0) {
             currentBlock = 0;
-            rotation = -1;
-            if (nextA == 0) {
+            direction = -1;
+            if (firstAxis == 0) {
                 if (key == GLUT_LEFT_BUTTON) {
                     rotationAxis = axisZ;
-                    rotation = 1;
+                    direction = 1;
                 } else if (key == GLUT_RIGHT_BUTTON) {
                     rotationAxis = axisY;
                 }
-            } else if (nextA == 1) {
+            } else if (firstAxis == 1) {
                 if (key == GLUT_LEFT_BUTTON) {
                     rotationAxis = axisZ;
                 } else if (key == GLUT_RIGHT_BUTTON) {
@@ -153,20 +153,20 @@ int rotateAlong( int axis, int nextA, int nextB, int key ) {
                     rotationAxis = axisY;
                 }
             }
-        } else if (nextB == 1) {
+        } else if (secondAxis == 1) {
             currentBlock = 1;
-            rotation = 1;
-            if (nextA == 0) {
+            direction = 1;
+            if (firstAxis == 0) {
                 if (key == GLUT_LEFT_BUTTON) {
                     rotationAxis = axisZ;
                 } else if (key == GLUT_RIGHT_BUTTON) {
                     currentBlock = 0;
                     rotationAxis = axisY;
                 }
-            } else if (nextA == 1) {
+            } else if (firstAxis == 1) {
                 if (key == GLUT_LEFT_BUTTON) {
                     rotationAxis = axisZ;
-                    rotation = -1;
+                    direction = -1;
                 } else if (key == GLUT_RIGHT_BUTTON) {
                     rotationAxis = axisY;
                 }
@@ -174,17 +174,17 @@ int rotateAlong( int axis, int nextA, int nextB, int key ) {
         }
     // Rotate along Y
     } else if (axis == axisY) {
-        if (nextA == 0) {
-            rotation = 1;
+        if (firstAxis == 0) {
+            direction = 1;
             currentBlock = 0;
-            if (nextB == 0) {
+            if (secondAxis == 0) {
                 if (key == GLUT_LEFT_BUTTON) {
                     rotationAxis = axisZ;
-                    rotation = -1;
+                    direction = -1;
                 } else if (key == GLUT_RIGHT_BUTTON) {
                     rotationAxis = axisX;
                 }
-            } else if (nextB == 1) {
+            } else if (secondAxis == 1) {
                 if (key == GLUT_LEFT_BUTTON) {
                     rotationAxis = axisZ;
                 } else if (key == GLUT_RIGHT_BUTTON) {
@@ -192,20 +192,20 @@ int rotateAlong( int axis, int nextA, int nextB, int key ) {
                     rotationAxis = axisX;
                 }
             }
-        } else if (nextA == 1) {
-            rotation = -1;
+        } else if (firstAxis == 1) {
+            direction = -1;
             currentBlock = 1;
-            if (nextB == 0) {
+            if (secondAxis == 0) {
                 if (key == GLUT_LEFT_BUTTON) {
                     rotationAxis = axisZ;
                 } else if(key == GLUT_RIGHT_BUTTON) {
                     currentBlock = 0;
                     rotationAxis = axisX;
                 }
-            } else if (nextB == 1) {
+            } else if (secondAxis == 1) {
                 if (key == GLUT_LEFT_BUTTON) {
                     rotationAxis = axisZ;
-                    rotation = 1;
+                    direction = 1;
                 } else if (key == GLUT_RIGHT_BUTTON){
                     rotationAxis = axisX;
                 }
@@ -213,17 +213,17 @@ int rotateAlong( int axis, int nextA, int nextB, int key ) {
         }
     // Rotate along Z
     } else if (axis == axisZ) {
-        if (nextA == 0) {
+        if (firstAxis == 0) {
             currentBlock = 0;
-            rotation = 1;
-            if (nextB == 0) {
+            direction = 1;
+            if (secondAxis == 0) {
                 if (key == GLUT_LEFT_BUTTON) {
                     rotationAxis = axisX;
-                    rotation = -1;
+                    direction = -1;
                 } else if (key == GLUT_RIGHT_BUTTON) {
                     rotationAxis = axisY;
                 }
-            } else if (nextB == 1) {
+            } else if (secondAxis == 1) {
                 if (key == GLUT_LEFT_BUTTON){
                     rotationAxis = axisX;
                 } else if (key == GLUT_RIGHT_BUTTON) {
@@ -231,79 +231,73 @@ int rotateAlong( int axis, int nextA, int nextB, int key ) {
                     rotationAxis = axisY;
                 }
             }
-        } else if (nextA == 1) {
-            rotation = -1;
+        } else if (firstAxis == 1) {
+            direction = -1;
             currentBlock = 1;
-            if (nextB == 0) {
+            if (secondAxis == 0) {
                 if (key == GLUT_LEFT_BUTTON) {
                     rotationAxis = axisX;
                 } else if (key == GLUT_RIGHT_BUTTON) {
                     currentBlock = 0;
                     rotationAxis = axisY;
                 }
-            } else if (nextB == 1) {
+            } else if (secondAxis == 1) {
                 if (key == GLUT_LEFT_BUTTON) {
                     rotationAxis = axisX;
-                    rotation = 1;
+                    direction = 1;
                 } else if (key == GLUT_RIGHT_BUTTON) {
                     rotationAxis = axisY;
                 }
             }
         }
     }
-    return rotation;
+    return direction;
 }
 
 // MARK: - Perform Rotation
 
-int performRotation( int currX, int currY, int currZ, int nextX, int nextY, int nextZ, int key ) {
-    int rotation = 0;
-    if (currX == 0) {
-        if (currY == 0) {
-            if (currZ == 1) {
-                rotation = rotateAlong( axisX, nextY, nextZ, key );
+int getDirection( int currentX, int currentY, int currentZ, int nextX, int nextY, int nextZ, int key ) {
+    int direction = 0;
+    if (currentX == 0) {
+        if (currentY == 0) {
+            if (currentZ == 1) {
+                direction = setParameters( axisX, nextY, nextZ, key );
             }
-        } else if (currY == 1) {
-            if (currZ == 0) {
-                rotation = rotateAlong( axisZ, nextX, nextY, key );
-            } else if (currZ == 1) {
-                rotation = rotateAlong( axisZ, nextX, nextY, key );
-                rotation = rotation * -1;
+        } else if (currentY == 1) {
+            direction = setParameters( axisZ, nextX, nextY, key );
+            if (currentZ == 1) {
+                direction = direction * -1;
             }
         }
-    } else if (currX == 1) {
-        if (currY == 0) {
-            if (currZ == 0) {
-                rotation = rotateAlong( axisY, nextZ, nextX, key );
-                rotation = rotation * -1;
-            } else if (currZ == 1) {
-                rotation = rotateAlong( axisX, nextY, nextZ, key );
-                rotation = rotation * -1;
+    } else if (currentX == 1) {
+        if (currentY == 0) {
+            if (currentZ == 0) {
+                direction = setParameters( axisY, nextZ, nextX, key );
+            } else if (currentZ == 1) {
+                direction = setParameters( axisX, nextY, nextZ, key );
             }
-        } else if (currY == 1) {
-            if (currZ == 0) {
-                rotation = rotateAlong( axisY, nextZ, nextX, key );
-            } else if (currZ == 1) {
-                rotation = 0;
+            direction = direction * -1;
+        } else if (currentY == 1) {
+            if (currentZ == 0) {
+                direction = setParameters( axisY, nextZ, nextX, key );
             }
         }
     }
-    return rotation;
+    return direction;
 }
 
 // MARK: - Select Cube Face
 
 int cubeSelector( int a, int b, int key ) {
-    int rotation = 0;
+    int cube;
+    int direction = 0;
+    int temp = 0;
     int nextX = -1;
     int nextY = -1;
     int nextZ = -1;
-    int temp = 0;
     unsigned char point[3];
-    int cube;
-
+    
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-
     glUniform1i( selectedCube, 1 );
 
     for (int i = 0 ; i < numCubeVertices ; i++) {
@@ -327,7 +321,6 @@ int cubeSelector( int a, int b, int key ) {
     cube = (k * 4) + (l * 2) + m;
    
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    
     glUniform1i( selectedFace, 1 );
 
     temp = 0;
@@ -354,9 +347,7 @@ int cubeSelector( int a, int b, int key ) {
         for (int j = 0 ; j < 2; j++) {
             for (int k = 0 ; k < 2 ; k++) {
                 if (currentCubePos[i][j][k] == cube) {
-                    nextX = i;
-                    nextY = j;
-                    nextZ = k;
+                    nextX = i; nextY = j; nextZ = k;
                     break;
                 }
             }
@@ -364,11 +355,10 @@ int cubeSelector( int a, int b, int key ) {
     }
    
     // Rotate with appropriate axes
-    rotation = performRotation( k, l, m, nextX, nextY, nextZ, key );
-    
+    direction = getDirection( k, l, m, nextX, nextY, nextZ, key );
     glutPostRedisplay();
 
-    return rotation;
+    return direction;
 }
 
 // MARK: - Randomly Initialize Cube
@@ -385,7 +375,7 @@ int cubeSelector( int a, int b, int key ) {
         } else {
             direction = -1;
         }
-        rubiksCubeRotation( direction, currentBlock, rotationAxis );
+        setNextPositions( direction, currentBlock, rotationAxis );
         for (int i = 0 ; i < 2 ; i++) {
             for (int j = 0 ; j < 2 ; j++) {
                 if (rotationAxis == axisX) {
@@ -408,7 +398,7 @@ int cubeSelector( int a, int b, int key ) {
             }
         }
     }
-    rotatePhase = 0;
+    rotateAngle = 0;
 }*/
 
 // MARK: - Initialization
@@ -486,9 +476,9 @@ void display() {
     
     const vec3 displacement( 0, 0, 0);
     mat4  model_view = ( Translate( displacement ) *
-                        RotateX( Theta[axisX] + rotateX ) *
+                        RotateX( Theta[axisX] + aspectX ) *
                         RotateY( Theta[axisY] ) *
-                        RotateZ( Theta[axisZ] + rotateZ ) );
+                        RotateZ( Theta[axisZ] + aspectZ ) );
     
     glUniformMatrix4fv( ModelView, 1, GL_TRUE, model_view );
 
@@ -555,17 +545,17 @@ void helpMenu() {
 void mouse( int key, int state, int x, int y ) {
     int direction;
     
-    if (state == GLUT_DOWN && rotatePhase == 0) {
+    if (state == GLUT_DOWN && rotateAngle == 0) {
         // Set the current block, rotation axis and direction
         direction = cubeSelector(x, y, key);
         if (direction < 0) {
-            rubiksCubeRotation(direction, currentBlock, rotationAxis);
-            rotatePhase = 180;
-            cubeSideRotation(-5);
+            setNextPositions(direction, currentBlock, rotationAxis);
+            rotateAngle = 180;
+            rotateCube(-5);
         } else if (direction > 0) {
-            rubiksCubeRotation(direction, currentBlock, rotationAxis);
-            rotatePhase = 0;
-            cubeSideRotation(5);
+            setNextPositions(direction, currentBlock, rotationAxis);
+            rotateAngle = 0;
+            rotateCube(5);
         }
     }
 }
@@ -575,16 +565,16 @@ void mouse( int key, int state, int x, int y ) {
 void arrowKeys( int key, int a, int b ) {
     switch (key) {
         case GLUT_KEY_LEFT:
-            rotateZ += 4;
+            aspectZ += 5;
             break;
         case GLUT_KEY_RIGHT:
-            rotateZ -= 4;
+            aspectZ -= 5;
             break;
         case GLUT_KEY_UP:
-            rotateX += 4;
+            aspectX += 5;
             break;
         case GLUT_KEY_DOWN:
-            rotateX -= 4;
+            aspectX -= 5;
             break;
     }
     glutPostRedisplay();
@@ -595,8 +585,8 @@ void arrowKeys( int key, int a, int b ) {
 void keyboard( unsigned char key, int x, int y ) {
     switch (key) {
         case 'i': case 'I':
-            rotateX = 135.0;
-            rotateZ = -45.0;
+            aspectX = 135.0;
+            aspectZ = -45.0;
             break;
         case 'r': case 'R':
             //randomInitialization(5);
--- Assignment2/Assignment2/main.h
+++ Assignment2/Assignment2/main.h
@@ -10,19 +10,19 @@ using namespace std;
 typedef Angel::vec4  color4;
 typedef Angel::vec4  point4;
 
-// Rubik's Cube specifications
+// Rubik's Cube variables
 const int numCubes = 8;
 const int cubeFaces = 6;
 const int faceVertices = 6;
 const int numOneCubeVertices = faceVertices * cubeFaces;
 const int numCubeVertices = numOneCubeVertices * numCubes;
 
-// Animation Speed */
+// Animation speed
 const int animationSpeed = 10;
 
-// Default position of the cube
-double rotateX = 135.0;
-double rotateZ = -45.0;
+// Default view aspect of the cube
+double aspectX = 135.0;
+double aspectZ = -45.0;
 double scaleFactor = 1.0;
 
 enum { axisX = 0, axisY = 1, axisZ = 2, numAxis = 3 };
@@ -37,10 +37,10 @@ point4 vertices[numCubeVertices];
 int currentCubePos[2][2][2];
 int nextCubePos[2][2][2];
 
-// Cube rotation-level variables; specify the current orientation of the cube
+// Cube rotation variables
 int rotationAxis = axisY; 
 int currentBlock = 1;
-int rotatePhase = 0;
+int rotateAngle = 0;
 
 // Uniform location
 int ModelView;
@@ -49,83 +49,23 @@ int edge;
 int currentCube;
 int vRotation[numCubes];
 
-// Variable for the shader
+// Variables for the shader
 int selectedCube;
 int selectedFace;
 int cubeColor[numCubes];
 
-int cubeSelector(int x, int y, int button);
-int performRotation(int currX, int currY, int currZ, int nextX, int nextY, int nextZ, int key);
-int rotateAlong(int axis, int kj, int lj, int button);
+// Function definitions
+int cubeSelector(int a, int b, int key);
+int performRotation(int currentX, int currentY, int currentZ, int nextX, int nextY, int nextZ, int key);
+int setParameters(int axis, int firstAxis, int secondAxis, int key);
 void cubeCreator(point4 *cube, color4 *color);
-void cubeTranslation(point4 *cube, vec3 displacement);
-void rubiksCube(void);
+void rubiksCube();
 void randomInitialization(int numRotations);
-void rubiksCubeRotation(int direction, int block, int axis);
+void setNextPositions(int direction, int subcube, int axis);
+void quad( point4 *cube, point4 *vertices, int a, int b, int c, int d );
 
 // MARK: - Cube Vertices
-
 void cubeCreator(point4 *cube, color4 *color) {
-    /*
-    const int NumVerticesCube = 36;
-    point4 pointsCube[NumVerticesCube];
-    color4 colors[NumVerticesCube];
-
-    // Vertices of a unit cube
-    point4 verticesCube[8] = {
-        point4( -0.5, -0.5,  0.5, 1.0 ),
-        point4( -0.5,  0.5,  0.5, 1.0 ),
-        point4(  0.5,  0.5,  0.5, 1.0 ),
-        point4(  0.5, -0.5,  0.5, 1.0 ),
-        point4( -0.5, -0.5, -0.5, 1.0 ),
-        point4( -0.5,  0.5, -0.5, 1.0 ),
-        point4(  0.5,  0.5, -0.5, 1.0 ),
-        point4(  0.5, -0.5, -0.5, 1.0 )
-    };
-
-    // RGBA colors
-    color4 vertex_colors[8] = {
-        color4( 0.0, 0.0, 0.0, 1.0 ),  // black
-        color4( 1.0, 0.0, 0.0, 1.0 ),  // red
-        color4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
-        color4( 0.0, 1.0, 0.0, 1.0 ),  // green
-        color4( 0.0, 0.0, 1.0, 1.0 ),  // blue
-        color4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
-        color4( 1.0, 1.0, 1.0, 1.0 ),  // white
-        color4( 0.0, 1.0, 1.0, 1.0 )   // cyan
-    };
-
-    // Array of rotation angles (in degrees) for each coordinate axis
-    enum { Xaxis = 0, Yaxis = 1, Zaxis = 2, NumAxes = 3 };
-    int Axis = Xaxis;
-
-    GLfloat Theta[NumAxes] = { 0.0, 0.0, 0.0 };
-
-    // Model-view, projection and color matrices uniform location
-    GLuint ModelView, Projection, Color;
-    GLuint vao[3];
-
-    // quad generates two triangles for each face
-    int Index = 0;
-    void quad( int a, int b, int c, int d ) {
-        pointsCube[Index] = verticesCube[a]; Index++;
-        pointsCube[Index] = verticesCube[b]; Index++;
-        pointsCube[Index] = verticesCube[c]; Index++;
-        pointsCube[Index] = verticesCube[a]; Index++;
-        pointsCube[Index] = verticesCube[c]; Index++;
-        pointsCube[Index] = verticesCube[d]; Index++;
-    }
-
-    // generate 12 triangles: 36 vertices
-    void cube() {
-        quad( 1, 0, 3, 2 );
-        quad( 2, 3, 7, 6 );
-        quad( 3, 0, 4, 7 );
-        quad( 6, 5, 1, 2 );
-        quad( 4, 5, 6, 7 );
-        quad( 5, 4, 0, 1 );
-    }
-*/
 	point4 vertices[8];
 
 	vertices[0] = point4(-0.5, 0.5, -0.5, 1.0);
@@ -187,5 +127,6 @@ void cubeCreator(point4 *cube, color4 *color) {
 	cube[33] = vertices[6];
 	cube[34] = vertices[7];
 	cube[35] = vertices[3];
-    
 }
+
+
